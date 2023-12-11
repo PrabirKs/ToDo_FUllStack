800049e7d276fbafb5e741b5a371e9d875ab4139
PrabirKs <pkxsahoo@gmail.com>
2023-12-11 19:02:25 +0530

    deploy trial 3

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState, useEffect } from "react";
 
-const API_BASE = "http://localhost:3001";
+const API_BASE = `${window.location.origin}`;
 
 function App() {
   const [todos, setTodos] = useState([]);
